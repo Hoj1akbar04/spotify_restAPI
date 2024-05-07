b494663828e5ffa71c851b968dc45c8c89e8687f
Hojiakbar <hojiakbaribodullayev04@gmail.com>
2024-05-08 01:24:20 +0500

    add Searching and Filters

--- music/views.py
+++ music/views.py
@@ -7,6 +7,8 @@ from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.pagination import LimitOffsetPagination
 
 
 class LandingPageView(APIView):
@@ -21,18 +23,27 @@ class ArtistAPIViewSet(ModelViewSet):
     queryset =Artist.objects.all()
     serializer_class = ArtistSerializer
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    pagination_class = LimitOffsetPagination
+    #permission_classes = (IsAuthenticated, )
 
 
 class AlbumAPIViewSet(ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title', 'artist__name')
+    pagination_class = LimitOffsetPagination
+    #permission_classes = (IsAuthenticated, )
 
 
 class SongAPIViewSet(ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    pagination_class = LimitOffsetPagination
+    search_fields = ('title', 'album__title', 'album__artist__name',)
+    #permission_classes = (IsAuthenticated,)
